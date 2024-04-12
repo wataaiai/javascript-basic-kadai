09374cb217e03fb869e9dfdfb8aba3db0af59b27
wataaiai <161330815+wataaiai@users.noreply.github.com>
2024-04-12 16:23:35 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date= new Date();
+
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const year = date.getFullYear();
+
+console.log(year + '年' + month + '月' + day + '日');
+
+
